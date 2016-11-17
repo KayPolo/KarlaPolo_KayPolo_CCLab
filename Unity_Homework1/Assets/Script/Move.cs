73ad8b039b9545e55e93cda3ed09c9592dc005dd
Karla Polo-Garcia <polok534@newschool.edu>
2016-11-17 18:51:44 -0500

    Unity Homework 1

---
+++ Unity_Homework1/Assets/Script/Move.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+	}
+
+	void Update () {
+
+
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			moveVertical = 1;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			moveVertical = -1;
+		} else {
+			moveVertical = 0;
+		}
+
+		if (Input.GetKeyDown ("space")){
+			transform.Translate(Vector3.up * 35 * Time.deltaTime, Space.World);
+		} 
+
+		if (Input.GetKeyDown ("space") && Input.GetKey (KeyCode.UpArrow)){
+			transform.Translate(Vector3.up * 35 * Time.deltaTime, Space.World);
+			moveVertical = 5;
+		} 
+
+		if (Input.GetKeyDown ("space") && Input.GetKey (KeyCode.DownArrow)){
+			transform.Translate(Vector3.up * 35 * Time.deltaTime, Space.World);
+			moveVertical = -5;
+		} 
+
+
+		if (Input.GetKeyDown ("space") && Input.GetKey (KeyCode.RightArrow)){
+			transform.Translate(Vector3.up * 35 * Time.deltaTime, Space.World);
+			moveHorizontal = 5;
+		} 
+
+		if (Input.GetKeyDown ("space") && Input.GetKey (KeyCode.LeftArrow)){
+			transform.Translate(Vector3.up * 35 * Time.deltaTime, Space.World);
+			moveHorizontal = -3;
+		} 
+
+
+		//		Debug.log("MoveVertical" + moveVertical);
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+
+		rb.AddForce (movement);
+	}
+
+	void LateUpdate(){
+	}
+
+
+	void OnTriggerEnter (Collider coll){
+		if (coll.gameObject.CompareTag ("Collectable")) {
+			coll.gameObject.SetActive (false);
+		}
+
+	}
+}
---
+++ Unity_Homework1/Assets/Script/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	private Vector3 rotateDirection = new Vector3(180,90,45);
+
+	void Update () {
+		transform.Rotate (rotateDirection * Time.deltaTime);
+	}
+}
+
+
+
+
+
